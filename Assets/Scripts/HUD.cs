515dffec85b68efb0891d713c527199cb6787ce4
WertiUni <35919403+WertiUni@users.noreply.github.com>
2018-02-26 12:11:08 +0000

    Timer
    Added countdown to hud and moved score to game logic

--- Assets/Scripts/HUD.cs
+++ Assets/Scripts/HUD.cs
@@ -7,6 +7,7 @@ public class HUD : MonoBehaviour {
 	public Text livesText; // text object to display the number of lives
 	public Text gameOverText; // text object to display game over message
     public Text scoreText; // text object to display score
+    public Text levelTimeText;
 
 
 	Player playerComponent;
@@ -27,10 +28,24 @@ public class HUD : MonoBehaviour {
 		// update the display for the player's number of lives
 		livesText.text = "Lives: "+playerComponent.Lives;
 
-        scoreText.text = "Score: " + playerComponent.Score;
+        scoreText.text = "Score: " + gameLogicComponent.Score;
 
-		// if game over, then display game over text
-		if (gameLogicComponent.gameOver == true) {
+        if (gameLogicComponent.LevelTime >= 100)
+        {
+            levelTimeText.text = "" + (int)gameLogicComponent.LevelTime;
+        } else if (gameLogicComponent.LevelTime >= 10)
+        {
+            levelTimeText.text = "0" + (int)gameLogicComponent.LevelTime;
+        } else if (gameLogicComponent.LevelTime >= 1)
+        {
+            levelTimeText.text = "00" + (int)gameLogicComponent.LevelTime;
+        } else
+        {
+            levelTimeText.text = "000";
+        }
+
+            // if game over, then display game over text
+            if (gameLogicComponent.gameOver == true) {
 			gameOverText.gameObject.SetActive (true);
 		}
 	}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,10 +11,7 @@ public class Player : MonoBehaviour {
 	private Vector3 moveDirection = Vector3.zero;
 
 	public int Lives = 3; // number of lives the player hs
-
-    public int Score = 0; // score
-
-
+    
 	Vector3 start_position; // start position of the player
 
 
