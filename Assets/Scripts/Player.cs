bcf8be14a5ea673392d68955e3202f7bae322953
JonDuncanson-abertay <1600538@uad.ac.uk>
2018-02-26 13:04:06 +0000

    Begin scene merge 1
    Scene merge 1.
    Changed some player stuff.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -11,12 +11,8 @@ public class Player : MonoBehaviour {
 	private Vector3 moveDirection = Vector3.zero;
 
 	public int Lives = 3; // number of lives the player hs
-<<<<<<< HEAD
-    
-=======
 
     private Rigidbody rb = null;
->>>>>>> pr/1
 	Vector3 start_position; // start position of the player
 
 
