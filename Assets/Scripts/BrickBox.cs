e7fc2201cf56f5a2029dc82288e8806e39bcbc68
JonDuncanson-abertay <1600538@uad.ac.uk>
2018-02-26 12:27:15 +0000

    BrickBox started
    Added BrickBox prefab.
    Added BrickBox script.

---
+++ Assets/Scripts/BrickBox.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickBox : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+        gameObject.SetActive(true);
+	}
+
+    /*void OnCollisionEnter(Collision collision)
+    {
+        //if (gameObject.activeSelf)
+        Debug.Log("oh boi");
+        if (collision.gameObject.tag == "Player")
+        {
+            Debug.Log("oh boi");
+        }
+    }*/
+	
+	// Update is called once per frame
+	void Update () {
+    }
+}
